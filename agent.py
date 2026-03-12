1de4cc7cd814eb2ffca669028ac0e60f75249c12
Ajani Hickling <ajanih@bu.edu>
2026-03-12 14:03:27 +0200

    made random movement function more robust

--- agent.py
+++ agent.py
@@ -16,7 +16,7 @@ class MonteCarloChessAgent(object):
 		#TODO: complete
 		# Captures > Check-giving moves > Random moves
 		# Enforce logic for higher value captures prioritised when multiple capture moves available? May not be necessary.
-		return 0,0,0,0
+		return random_move(self, board)
 		
 
 class RandomChessAgent(object):
@@ -24,8 +24,8 @@ class RandomChessAgent(object):
 		self.color = color
 
 	def get_next_move(self, board:RawChessBoard, is_init=False): #must return src_row, src_col, dest_row, dest_col
-		raw_board = board.uboard # get instance of RawChessBoard to work with
-		return random_move(self, raw_board)
+		# raw_board = board.uboard # get instance of RawChessBoard to work with
+		return random_move(self, board)
 	
 
 # HELPER FUNCTIONS
@@ -33,8 +33,15 @@ def random_move(self: MonteCarloChessAgent | RandomChessAgent, board:RawChessBoa
 		# Random Chess Agent acts randomly -- random piece selection, random move selection from that piece's available moveset
 		# MCTS Agent does NOT act randomly, but needs random move selection if no captures or check-giving moves are available. Can use this function in that case.
 		
-		all_available_moves = board.get_playable_moves(self.color)	# Gets all playable moves at given state of the board for the agent's color
-		selected_piece_moves = choice(all_available_moves)		# Randomly selects a piece, as well as all its move choices
+		all_available_moves = board.uboard.get_playable_moves(self.color)	# Gets all playable moves at given state of the board for the agent's color
+
+		# Account for cases where moves are not possible (avoid IndexError from choice() function)
+		# Filters invalid moves
+		valid_moves = [mv for mv in all_available_moves if len(mv["moves"]) > 0]
+		if len(valid_moves) == 0:
+			raise Exception("No valid moves available for agent of color {0}".format(self.color))
+
+		selected_piece_moves = choice(valid_moves)		# Randomly selects a piece, as well as all its move choices
 		selected_move = choice(selected_piece_moves["moves"])	# Randomly selects a move from the selected piece's move choices
 		return selected_piece_moves["row"], selected_piece_moves["col"], selected_move[0], selected_move[-1]
 		#NB: selected_piece moves dict contains row & col (initial position of selected piece); selected_move is a tuple containing new row and new col (in that order)
